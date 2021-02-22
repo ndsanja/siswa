b35dff396b608c9e53c8156465134d055fcfdc89
ndsanja <ndsanja@gmail.com>
2021-02-22 07:30:35 +0700

    set bg white

--- components/MainContent.jsx
+++ components/MainContent.jsx
@@ -3,7 +3,7 @@ import Card from "./Card";
 
 export default function MainContent(props) {
   return (
-    <div className="w-9/12 absolute right-0 top-0 mt-24 bg-white">
+    <div className="w-9/12 absolute right-0 top-0 pt-24 bg-white">
       <div className="container p-10 ">{props.children}</div>
     </div>
   );
