157231d236860ffef92cc383638a430abc0d000d
ndsanja <ndsanja@gmail.com>
2021-03-04 03:29:41 +0700

    menyelesaikan tabs pelajaran

--- components/Sekolah/Pelajaran/Latihan.jsx
+++ components/Sekolah/Pelajaran/Latihan.jsx
@@ -6,7 +6,11 @@ import Flex from "../../Flex";
 
 export default function Silabus(props) {
   return (
-    <Flex className="flex-col space-y-8">
+    <TabPanel
+      className={`${
+        props.openTab == "latihan" ? `flex` : `hidden`
+      } flex-col space-y-8`}
+    >
       <Card className="rounded-xl shadow-z12-light-shadow p-8">
         <Flex className="flex-row items-center space-x-4">
           <div className="w-24 h-32 bg-primary-main"></div>
@@ -16,59 +20,54 @@ export default function Silabus(props) {
           </h1>
         </Flex>
       </Card>
-      <TabPanel
-        className={`${
-          props.openTab == "latihan" ? `flex` : `hidden`
-        } flex-col space-y-8`}
-      >
-        <ListLatihan>
-          <ListItemsLatihan
-            nama="Pelajaran 1"
-            deskripsi="Kuis"
-            status="Terkumpul"
-            deadline="17 Feb"
-            jam="20.00 WIB"
-            bobot="2%"
-            level="menengah"
-          />
-          <ListItemsLatihan
-            nama="Pelajaran 1"
-            deskripsi="Kuis"
-            status="Terkumpul"
-            deadline="17 Feb"
-            jam="20.00 WIB"
-            bobot="2%"
-            level="menengah"
-          />
-          <ListItemsLatihan
-            nama="Pelajaran 1"
-            deskripsi="Kuis"
-            status="Terkumpul"
-            deadline="17 Feb"
-            jam="20.00 WIB"
-            bobot="2%"
-            level="menengah"
-          />
-          <ListItemsLatihan
-            nama="Pelajaran 1"
-            deskripsi="Kuis"
-            status="Terkumpul"
-            deadline="17 Feb"
-            jam="20.00 WIB"
-            bobot="2%"
-            level="menengah"
-          />
-          <ListItemsLatihan
-            nama="Pelajaran 1"
-            deskripsi="Kuis"
-            status="Terkumpul"
-            deadline="17 Feb"
-            jam="20.00 WIB"
-            bobot="2%"
-            level="menengah"
-          />
-        </ListLatihan>
-      </TabPanel>
-    </Flex>
+
+      <ListLatihan>
+        <ListItemsLatihan
+          nama="Pelajaran 1"
+          deskripsi="Kuis"
+          status="Terkumpul"
+          deadline="17 Feb"
+          jam="20.00 WIB"
+          bobot="2%"
+          level="menengah"
+        />
+        <ListItemsLatihan
+          nama="Pelajaran 1"
+          deskripsi="Kuis"
+          status="Terkumpul"
+          deadline="17 Feb"
+          jam="20.00 WIB"
+          bobot="2%"
+          level="menengah"
+        />
+        <ListItemsLatihan
+          nama="Pelajaran 1"
+          deskripsi="Kuis"
+          status="Terkumpul"
+          deadline="17 Feb"
+          jam="20.00 WIB"
+          bobot="2%"
+          level="menengah"
+        />
+        <ListItemsLatihan
+          nama="Pelajaran 1"
+          deskripsi="Kuis"
+          status="Terkumpul"
+          deadline="17 Feb"
+          jam="20.00 WIB"
+          bobot="2%"
+          level="menengah"
+        />
+        <ListItemsLatihan
+          nama="Pelajaran 1"
+          deskripsi="Kuis"
+          status="Terkumpul"
+          deadline="17 Feb"
+          jam="20.00 WIB"
+          bobot="2%"
+          level="menengah"
+        />
+      </ListLatihan>
+    </TabPanel>
   );
 }
